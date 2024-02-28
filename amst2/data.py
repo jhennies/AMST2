aad2555511f08adc81d9f557b46e34e1d79beee5
jhennies <julianhennies@hotmail.de>
2024-02-28 17:30:33 +0100

    default resources

--- amst2/data.py
+++ amst2/data.py
@@ -147,7 +147,7 @@ def snk_stack_to_ome_zarr():
         sn_args.set_resources = dict(
             batch_to_ome_zarr=dict(
                 # I'm purposely not using snakemake's functionality here to determine mem_mb on-the-fly
-                mem_mb=int(np.ceil(estimate_mem_mb(data_h) * batch_size * 2)),
+                mem_mb=int(np.ceil(estimate_mem_mb(data_h) * batch_size * 8)),
                 time=10,
                 partition='htc-el8'
             )
