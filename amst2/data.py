f70fd9245c75b4464674157053d2aa4f5420e6d1
Julian <julianhennies@hotmail.de>
2024-02-28 16:22:53 +0100

    sync

--- amst2/data.py
+++ amst2/data.py
@@ -145,7 +145,7 @@ def snk_stack_to_ome_zarr():
                 partition='htc-el8'
             )
         )
-        sn_args.profile = os.path.join(src_dirpath, 'cluster', cluster)
+        sn_args.workflow_profile = os.path.join(src_dirpath, 'cluster', cluster)
         sn_args.executor = cluster
     args_to_api(sn_args, parser)
 
