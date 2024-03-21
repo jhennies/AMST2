9a30d13f2178beb0873dbe4d9bc83f22d4629a48
jhennies <julianhennies@hotmail.de>
2024-03-21 22:15:25 +0100

    alignment_preview fixed

--- amst2/snakemake_call_scripts/alignment_preview.py
+++ amst2/snakemake_call_scripts/alignment_preview.py
@@ -26,19 +26,19 @@ if __name__ == '__main__':
         #     transforms.extend(json.load(f))
 
     # Downsample the transformations
-    from squirrel.library.transformation import scale_sequential_affines
+    from squirrel.library.transformation import scale_sequential_affines, serialize_affine_sequence
     from squirrel.library.ome_zarr import get_ome_zarr_handle, get_scale_of_downsample_level
 
     input_ome_zarr_fileh = get_ome_zarr_handle(input_ome_zarr_filepath, mode='r')
-    scale = get_scale_of_downsample_level(input_ome_zarr_fileh, preview_downsample_level)
+    scale_full = get_scale_of_downsample_level(input_ome_zarr_fileh, 0)
+    scale_ds = get_scale_of_downsample_level(input_ome_zarr_fileh, preview_downsample_level)
+    scale = (np.array(scale_ds) / np.array(scale_full)).astype(int)
     assert scale[0] == scale[1] == scale[2], 'Implemented only for isotropic scaling!'
     scale = 1 / scale[0]
-    # if verbose:
-    print(f'transforms[4] = {transforms[0]}')
-    print(f'scale = {scale}')
+    if verbose:
+        print(f'scale = {scale}')
+    transforms = serialize_affine_sequence(transforms, param_order='M', out_param_order='M', verbose=verbose)
     transforms = scale_sequential_affines(transforms, scale, xy_pivot=(0., 0.))
-    # if verbose:
-    print(f'transforms[1] = {transforms[0]}')
 
     # Serialize and apply the transformations
     from squirrel.library.transformation import apply_stack_alignment
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         input_ome_zarr_dataseth,
         input_ome_zarr_dataseth.shape,
         transforms,
-        no_adding_of_transforms=False,
+        no_adding_of_transforms=True,
         xy_pivot=(0., 0.),
         param_order='M',
         verbose=verbose
