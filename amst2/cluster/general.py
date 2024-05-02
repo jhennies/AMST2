4d05d7e3639eee37cc8d6346b7c1b9d9b0537848
jhennies <julianhennies@hotmail.de>
2024-05-02 16:16:57 +0200

    adjustable memory requirement for snakejobs

---
+++ amst2/cluster/general.py
@@ -0,0 +1,26 @@
+
+def set_resources(
+        sn_args, rule_names, mem_defaults, runtime_defaults,
+        mem_args=None, runtime_args=None
+):
+
+    assert len(rule_names) == len(mem_defaults) == len(runtime_defaults)
+
+    def _split_args(args):
+        result_dict = dict()
+        if args is not None:
+            for entry in args:
+                rule_name, value = str.split(entry, ':')
+                result_dict[rule_name] = int(value)
+        return result_dict
+
+    mem_dict = _split_args(mem_args)
+    runtime_dict = _split_args(runtime_args)
+
+    sn_args.set_resources = {
+        dict(
+            mem_mb=mem_defaults[idx] if rule_name not in mem_dict else mem_dict[rule_name],
+            runtime=runtime_defaults[idx] if rule_name not in runtime_dict else runtime_dict[rule_name]
+        )
+        for idx, rule_name in enumerate(rule_names)
+    }
